4fb6cadf38f6daa01ea2c7910c3f7940a6450daa
Leon Bogaert <leon@tim-online.nl>
2013-09-17 20:37:38 +0200

    Don't force start of puppet client service on boot

--- manifests/client/service.pp
+++ manifests/client/service.pp
@@ -4,7 +4,7 @@ class puppet::client::service inherits puppet::service {
 		ensure     => stopped,
 		hasstatus  => true,
 		hasrestart => true,
-		enable     => true,
+		enable     => false, # don't start on boot
 		require    => Class['puppet::package'],
 		subscribe  => File['/etc/puppet/puppet.conf']
 	}
